85dbcede67f4ba3354b588aada7b5bc2569c4c2a
Tomas Antonio Garcia Cornejo <tomas_garcia_cornejo@yahoo.com>
2025-10-03 18:59:24 -0600

    Ajuste de configuraciones

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,4 +1,3 @@
-// backend/src/app.ts
 import express, { Request, Response } from 'express';
 import helmet from 'helmet';
 import cors from 'cors';
@@ -6,20 +5,18 @@ import morgan from 'morgan';
 
 // 1. Configuración del Servidor
 const app = express();
+
 // Puerto: busca en las variables de entorno o usa 3001 por defecto
 const PORT = process.env.PORT || 3001; 
 
 // 2. Middlewares Globales
-// Ejecución secuencial (similar al pipeline de peticiones de .NET)
 app.use(helmet());            // Seguridad (headers HTTP)
 app.use(cors());              // Permite la comunicación con el frontend
 app.use(morgan('dev'));       // Logging en consola
 app.use(express.json());      // Permite leer el cuerpo de las peticiones en formato JSON (como un [FromBody])
 
 // 3. Tu Primer Endpoint (Ruta)
-// Esto es similar al método [HttpGet] en un controlador de ASP.NET
 app.get('/', (req: Request, res: Response) => {
-    // res.json() envía una respuesta JSON con código de estado 200 (equivalente a return Ok(data);)
     res.json({ 
         message: "¡AtlasSync API funcionando! Versión: 1.0",
         path: "/api/files (Próxima ruta)"
